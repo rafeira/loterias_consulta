ef7b700a6f19c9b10d7214c3e721194f63761a25
Rafael Vieira <rafael12wq@gmail.com>
2022-04-17 18:30:58 -0300

    Implement consults controller

--- app/controllers/consults_controller.rb
+++ app/controllers/consults_controller.rb
@@ -1,7 +1,31 @@
 class ConsultsController < ApplicationController
-    def new
-        @raffles = Raffle.all
-        render 'consults/new'
-    end
+	before_action :set_input_values, only: [:search]
+	def search
+		@errors = []
+		set_errors if params["0"]
+		@raffles = []
+		if @errors.empty?
+			@raffles = Raffle.all
+		end
 
+		render 'consults/search'
+	end
+	private
+		def numbers_params
+			params.permit("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15")
+		end
+		def set_input_values
+			@values = numbers_params.values
+		end
+
+		def set_errors
+			if numbers_params.values.any?{|v| v.empty?}
+				@errors << "Todos os campos devem ser preenchidos!"
+			elsif numbers_params.values.to_set.length < 15
+				@errors << "Os valores não podem ser repetidos!"
+			end
+			if numbers_params.values.any?{|v| v.to_i <= 0 || v.to_i > 25}
+				@errors << "Insira valores válidos!"
+			end
+		end
 end
